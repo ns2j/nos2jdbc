bb44c07dcd07b6309ceb3db9235dd9f8328bab9a
ns2j <31337551+ns2j@users.noreply.github.com>
2017-08-26 13:07:18 +0900

    add jdbc's unimplemented method

--- nos2jdbc-core/src/main/java/org/seasar/extension/datasource/impl/SelectableDataSourceProxy.java
+++ nos2jdbc-core/src/main/java/org/seasar/extension/datasource/impl/SelectableDataSourceProxy.java
@@ -18,6 +18,8 @@ package org.seasar.extension.datasource.impl;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
@@ -162,5 +164,17 @@ public class SelectableDataSourceProxy implements DataSource {
      */
     public void setLoginTimeout(int seconds) throws SQLException {
         getDataSource().setLoginTimeout(seconds);
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+	throw new SQLFeatureNotSupportedException();
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+	throw new SQLFeatureNotSupportedException();
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+	throw new SQLFeatureNotSupportedException();
     }
 }
