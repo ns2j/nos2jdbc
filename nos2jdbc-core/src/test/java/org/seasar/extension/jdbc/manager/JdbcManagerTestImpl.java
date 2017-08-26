dc2f13bb3ae9facf5183f11ccc2aa804e396654b
ns2j <31337551+ns2j@users.noreply.github.com>
2017-08-26 13:29:43 +0900

    remove container dependencies

---
+++ nos2jdbc-core/src/test/java/org/seasar/extension/jdbc/manager/JdbcManagerTestImpl.java
@@ -0,0 +1,46 @@
+package org.seasar.extension.jdbc.manager;
+
+import javax.transaction.TransactionManager;
+
+import org.seasar.extension.jdbc.dialect.StandardDialect;
+import org.seasar.extension.jdbc.meta.ColumnMetaFactoryImpl;
+import org.seasar.extension.jdbc.meta.EntityMetaFactoryImpl;
+import org.seasar.extension.jdbc.meta.PropertyMetaFactoryImpl;
+import org.seasar.extension.jdbc.meta.TableMetaFactoryImpl;
+import org.seasar.extension.jta.TransactionManagerImpl;
+import org.seasar.extension.jta.TransactionSynchronizationRegistryImpl;
+import org.seasar.framework.convention.impl.PersistenceConventionImpl;
+import org.seasar.framework.mock.sql.MockDataSource;
+
+public class JdbcManagerTestImpl extends JdbcManagerImpl {
+    private TransactionManager transactionManager;
+    
+    public JdbcManagerTestImpl() {
+	super();
+	setDialect(new StandardDialect());
+	transactionManager = new TransactionManagerImpl();
+	setSyncRegistry(new TransactionSynchronizationRegistryImpl(
+		transactionManager));
+	setDataSource(new MockDataSource());
+
+	PersistenceConventionImpl convention = new PersistenceConventionImpl();
+	EntityMetaFactoryImpl emFactory = new EntityMetaFactoryImpl();
+	emFactory.setPersistenceConvention(convention);
+	TableMetaFactoryImpl tableMetaFactory = new TableMetaFactoryImpl();
+	tableMetaFactory.setPersistenceConvention(convention);
+	emFactory.setTableMetaFactory(tableMetaFactory);
+
+	PropertyMetaFactoryImpl pFactory = new PropertyMetaFactoryImpl();
+	pFactory.setPersistenceConvention(convention);
+	ColumnMetaFactoryImpl cmFactory = new ColumnMetaFactoryImpl();
+	cmFactory.setPersistenceConvention(convention);
+	pFactory.setColumnMetaFactory(cmFactory);
+	emFactory.setPropertyMetaFactory(pFactory);
+	emFactory.initialize();
+	setEntityMetaFactory(emFactory);
+    }
+    
+    public TransactionManager getTransactionManager() {
+	return transactionManager;
+    }
+}
