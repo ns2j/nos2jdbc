f44420d9f45d175417a4b012c6b8638073e2ae04
igari <igari.y@gmail.com>
2019-03-27 14:33:27 +0900

    change to using jdk.javadoc.doclet

--- nos2jdbc-gen/src/main/java/org/seasar/extension/jdbc/gen/internal/meta/CommentDoclet.java
+++ nos2jdbc-gen/src/main/java/org/seasar/extension/jdbc/gen/internal/meta/CommentDoclet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2015 the Seasar Foundation and the Others.
+ * Copyright 2004-2015 the Seasar Foundation avnd the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,46 +15,110 @@
  */
 package org.seasar.extension.jdbc.gen.internal.meta;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FilterReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.util.ElementFilter;
 import javax.persistence.MappedSuperclass;
+import javax.tools.Diagnostic.Kind;
+
+import jdk.javadoc.doclet.Doclet;
+import jdk.javadoc.doclet.DocletEnvironment;
+import jdk.javadoc.doclet.Reporter;
 
 import org.seasar.extension.jdbc.EntityMeta;
 import org.seasar.extension.jdbc.PropertyMeta;
 import org.seasar.extension.jdbc.gen.internal.util.EntityMetaUtil;
 import org.seasar.extension.jdbc.gen.internal.util.PropertyMetaUtil;
+import org.seasar.framework.log.Logger;
 
-import com.sun.javadoc.AnnotationDesc;
-import com.sun.javadoc.ClassDoc;
-import com.sun.javadoc.Doclet;
-import com.sun.javadoc.FieldDoc;
-import com.sun.javadoc.RootDoc;
+import com.sun.source.doctree.DocCommentTree;
+import com.sun.source.util.DocTrees;
+
 
 /**
  * エンティティとプロパティのコメントを抽出する{@link Doclet}です。
  * 
  * @author taedium
  */
-public class CommentDoclet extends Doclet {
+public class CommentDoclet implements Doclet {
+    protected static Logger logger = Logger
+            .getLogger(CommentDoclet.class);
 
-    public static boolean start(RootDoc rootDoc) {
+    protected DocletEnvironment env;
+    
+    protected DocTrees docTrees;
+	
+	protected String charset;
+    
+	@Override
+    public boolean run(DocletEnvironment env) {
+		this.env = env;
         List<EntityMeta> entityMetaList = CommentDocletContext
                 .getEntityMetaList();
         if (entityMetaList == null) {
             throw new NullPointerException("entityMetaList");
         }
+        docTrees = env.getDocTrees();
         for (EntityMeta entityMeta : entityMetaList) {
-            ClassDoc classDoc = rootDoc.classNamed(entityMeta.getEntityClass()
+            TypeElement classDoc = env.getElementUtils().getTypeElement(entityMeta.getEntityClass()
                     .getName());
             if (classDoc == null) {
                 continue;
             }
+    		try {
             doEntityComment(classDoc, entityMeta);
+    		} catch (Exception ex) {
+    			logger.warn(ex);
+    			ex.printStackTrace();
+    		}
         }
         return true;
     }
+	
+	protected String getBody(Element e) {
+		if (e == null) {
+			logger.warn("getBody(): Element is null");
+			return "";
+		}
+		DocCommentTree docCommentTree = docTrees.getDocCommentTree(e);
+		if (docCommentTree == null) {
+			logger.debug("getBody(): DocElementTree is null: kind: " + e.getKind() + ": " + e);
+			return "";
+		}
+
+		String fullBodyUnicode = "" + docCommentTree.getFullBody();
+		String fullBody = null;
+		try {
+			fullBody = convertToOiginal(fullBodyUnicode);
+		} catch (IOException e1) {
+			logger.warn(e1);
+			e1.printStackTrace();
+		}
+		logger.debug(fullBodyUnicode);
+		logger.debug(fullBody);
+		return fullBody;
+	}
 
     /**
      * エンティティクラスのコメントを処理します。
@@ -63,14 +127,14 @@ public class CommentDoclet extends Doclet {
      * @param entityMeta
      *            エンティティメタデータ
      */
-    protected static void doEntityComment(ClassDoc classDoc,
+    protected void doEntityComment(TypeElement classDoc,
             EntityMeta entityMeta) {
-        EntityMetaUtil.setComment(entityMeta, classDoc.commentText());
+        EntityMetaUtil.setComment(entityMeta, getBody(classDoc));
 
-        Map<String, FieldDoc> fieldDocMap = getFieldDocMap(classDoc);
+        Map<String, VariableElement> fieldDocMap = getFieldDocMap(classDoc);
         for (PropertyMeta propertyMeta : entityMeta.getAllPropertyMeta()) {
             if (fieldDocMap.containsKey(propertyMeta.getName())) {
-                FieldDoc fieldDoc = fieldDocMap.get(propertyMeta.getName());
+                VariableElement fieldDoc = fieldDocMap.get(propertyMeta.getName());
                 doPropertyComment(fieldDoc, propertyMeta);
             }
         }
@@ -83,29 +147,29 @@ public class CommentDoclet extends Doclet {
      * @param propertyMeta
      *            プロパティメタデータ
      */
-    protected static void doPropertyComment(FieldDoc fieldDoc,
+    protected void doPropertyComment(VariableElement fieldDoc,
             PropertyMeta propertyMeta) {
-        PropertyMetaUtil.setComment(propertyMeta, fieldDoc.commentText());
+        PropertyMetaUtil.setComment(propertyMeta, getBody(fieldDoc));
     }
 
     /**
-     * フィールド名をキー、 {@link FieldDoc}を値とするマップを返します。
+     * フィールド名をキー、 {@link VariableElement}を値とするマップを返します。
      * 
      * @param classDoc
-     * @return フィールド名をキー、 {@link FieldDoc}を値とするマップ
+     * @return フィールド名をキー、 {@link VariableElement}を値とするマップ
      */
-    protected static Map<String, FieldDoc> getFieldDocMap(ClassDoc classDoc) {
-        Map<String, FieldDoc> fieldMap = new HashMap<String, FieldDoc>();
-        for (FieldDoc fieldDoc : classDoc.fields()) {
-            fieldMap.put(fieldDoc.name(), fieldDoc);
-        }
-        for (ClassDoc superclassDoc = classDoc.superclass(); !Object.class
-                .getName().equals(superclassDoc.qualifiedName()); superclassDoc = superclassDoc
-                .superclass()) {
+    protected Map<String, VariableElement> getFieldDocMap(TypeElement classDoc) {
+        Map<String, VariableElement> fieldMap = new HashMap<String, VariableElement>();
+        ElementFilter.fieldsIn(env.getElementUtils().getAllMembers(classDoc))
+        .forEach(e -> fieldMap.put(e.getSimpleName().toString(), e));
+
+        for (TypeElement superclassDoc = (TypeElement)((DeclaredType)classDoc.getSuperclass()).asElement();
+        		!Object.class.getName().equals(superclassDoc.getQualifiedName().toString());
+        		superclassDoc = (TypeElement)((DeclaredType)superclassDoc.getSuperclass()).asElement()) {
             if (isMappedSuperclass(superclassDoc)) {
-                for (FieldDoc fieldDoc : superclassDoc.fields()) {
-                    if (!fieldMap.containsKey(fieldDoc.name())) {
-                        fieldMap.put(fieldDoc.name(), fieldDoc);
+                for (VariableElement fieldDoc : ElementFilter.fieldsIn(env.getElementUtils().getAllMembers(superclassDoc))) {
+                    if (!fieldMap.containsKey(fieldDoc.getSimpleName().toString())) {
+                        fieldMap.put(fieldDoc.getSimpleName().toString(), fieldDoc);
                     }
                 }
             }
@@ -119,14 +183,195 @@ public class CommentDoclet extends Doclet {
      * @param classDoc
      * @return {@link MappedSuperclass}を表す場合{@code true}
      */
-    protected static boolean isMappedSuperclass(ClassDoc classDoc) {
-        for (AnnotationDesc desc : classDoc.annotations()) {
-            if (MappedSuperclass.class.getName().equals(
-                    desc.annotationType().qualifiedName())) {
-                return true;
-            }
-        }
-        return false;
+    protected boolean isMappedSuperclass(TypeElement classDoc) {
+        if (classDoc.getAnnotationsByType(MappedSuperclass.class).length == 0)
+        	return false;
+        return true;
+    }
+
+    Reporter reporter;
+
+    @Override
+    public void init(Locale locale, Reporter reporter) {
+    	reporter.print(Kind.NOTE, "Doclet using locale: " + locale);
+    	this.reporter = reporter;
+    }
+
+    @Override
+    public String getName() {
+    	return "CommentDoclet";
+    }
+
+    @Override
+    public Set<? extends Option> getSupportedOptions() {
+    	Option[] options = {
+    			new Option() {
+    				private final List<String> someOption = Arrays.asList(
+    						"--charset"
+    						);
+
+    				@Override
+    				public int getArgumentCount() {
+    					return 1;
+    				}
+
+    				@Override
+    				public String getDescription() {
+    					return "charset";
+    				}
+
+    				@Override
+    				public Option.Kind getKind() {
+    					return Option.Kind.STANDARD;
+    				}
+
+    				@Override
+    				public List<String> getNames() {
+    					return someOption;
+    				}
+
+    				@Override
+    				public String getParameters() {
+    					return "charset";
+    				}
+
+    				@Override
+    				public boolean process(String opt, List<String> arguments) {
+    					logger.debug(opt);
+    					arguments.forEach(logger::debug);
+    					charset = arguments.get(0);
+    					return true;
+    				}
+    			}
+    	};
+    	return new HashSet<>(Arrays.asList(options));
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+    	return null;
+    }
+
+    protected String convertToOiginal(String unicode) throws IOException {
+    	try (StringReader sr = new StringReader(unicode);
+    			BufferedReader br = new BufferedReader(new A2NFilter(sr));
+    			StringWriter sw = new StringWriter();
+    			BufferedWriter bw = new BufferedWriter(sw);
+    			) {
+    		String line;
+    		while ((line = br.readLine()) != null) {
+    			bw.write(line.toCharArray());
+    			bw.newLine();
+    		}
+    		bw.flush();
+    		return sw.toString().stripTrailing();
+    	}
+    }
+
+    // A copy of native2ascii A2NFilter
+    class A2NFilter extends FilterReader {
+    	// maintain a trailing buffer to hold any incompleted
+    	// unicode escaped sequences
+    	private char[] trailChars = null;
+    	Charset cs = Charset.forName(charset);
+    	CharsetEncoder encoder;
+
+    	public A2NFilter(Reader in) {
+    		super(in);
+    		encoder = cs.newEncoder();
+    	}
+
+    	public int read(char[] buf, int off, int len) throws IOException {
+    		int numChars = 0;        // how many characters have been read
+    		int retChars = 0;        // how many characters we'll return
+
+    		char[] cBuf = new char[len];
+    		int cOffset = 0;         // offset at which we'll start reading
+    		boolean eof = false;
+
+    		// copy trailing chars from previous invocation to input buffer
+    		if (trailChars != null) {
+    			for (int i = 0; i < trailChars.length; i++)
+    				cBuf[i] = trailChars[i];
+    			numChars = trailChars.length;
+    			trailChars = null;
+    		}
+
+    		int n = in.read(cBuf, numChars, len - numChars);
+    		if (n < 0) {
+    			eof = true;
+    			if (numChars == 0)
+    				return -1;              // EOF;
+    		} else {
+    			numChars += n;
+    		}
+
+    		for (int i = 0; i < numChars; ) {
+    			char c = cBuf[i++];
+
+    			if (c != '\\' || (eof && numChars <= 5)) {
+    				// Not a backslash, so copy and continue
+    				// Always pass non backslash chars straight thru
+    				// for regular encoding. If backslash occurs in
+    				// input stream at the final 5 chars then don't
+    				// attempt to read-ahead and de-escape since these
+    				// are literal occurrences of U+005C which need to
+    				// be encoded verbatim in the target encoding.
+    				buf[retChars++] = c;
+    				continue;
+    			}
+
+    			int remaining = numChars - i;
+    			if (remaining < 5) {
+    				// Might be the first character of a unicode escape, but we
+    				// don't have enough characters to tell, so save it and finish
+    				trailChars = new char[1 + remaining];
+    				trailChars[0] = c;
+    				for (int j = 0; j < remaining; j++)
+    					trailChars[1 + j] = cBuf[i + j];
+    				break;
+    			}
+    			// At this point we have at least five characters remaining
+
+    			c = cBuf[i++];
+    			if (c != 'u') {
+    				// Not a unicode escape, so copy and continue
+    				buf[retChars++] = '\\';
+    				buf[retChars++] = c;
+    				continue;
+    			}
+
+    			// The next four characters are the hex part of a unicode escape
+    			char rc = 0;
+    			boolean isUE = true;
+    			try {
+    				rc = (char) Integer.parseInt(new String(cBuf, i, 4), 16);
+    			} catch (NumberFormatException x) {
+    				isUE = false;
+    			}
+    			if (isUE && encoder.canEncode(rc)) {
+    				// We'll be able to convert this
+    				buf[retChars++] = rc;
+    				i += 4; // Align beyond the current uXXXX sequence
+    			} else {
+    				// We won't, so just retain the original sequence
+    				buf[retChars++] = '\\';
+    				buf[retChars++] = 'u';
+    				continue;
+    			}
+
+    		}
+    		return retChars;
+    	}
+
+    	public int read() throws IOException {
+    		char[] buf = new char[1];
+
+    		if (read(buf, 0, 1) == -1)
+    			return -1;
+    		else
+    			return (int) buf[0];
+    	}
     }
 
 }
