4a848447425cd1e3c50d7cf819fa3ce5a2106cd8
igari <igari.y@gmail.com>
2020-07-27 21:07:14 +0900

    not use deprecated method clazz.newInsance()

--- nos2jdbc-core/src/main/java/nos2jdbc/standalone/NoS2JdbcManager.java
+++ nos2jdbc-core/src/main/java/nos2jdbc/standalone/NoS2JdbcManager.java
@@ -18,6 +18,7 @@ package nos2jdbc.standalone;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,12 +31,13 @@ import org.seasar.extension.jdbc.DbmsDialect;
 import org.seasar.extension.jdbc.JdbcManager;
 import org.seasar.extension.jdbc.dialect.OracleDialect;
 import org.seasar.extension.jdbc.manager.JdbcManagerImpl;
-import org.seasar.extension.jta.TransactionManagerImpl;
 import org.seasar.extension.jta.TransactionSynchronizationRegistryImpl;
 import org.seasar.framework.exception.ClassNotFoundRuntimeException;
 import org.seasar.framework.exception.IORuntimeException;
 import org.seasar.framework.exception.IllegalAccessRuntimeException;
 import org.seasar.framework.exception.InstantiationRuntimeException;
+import org.seasar.framework.exception.InvocationTargetRuntimeException;
+import org.seasar.framework.exception.NoSuchMethodRuntimeException;
 
 import static org.seasar.framework.util.StringUtil.*;
 
@@ -48,46 +50,46 @@ public class NoS2JdbcManager {
     final static String PROPS_NAME = "nos2jdbc-datasource.properties";  
 
     private NoS2JdbcManager(String propertiesFilename, DataSource ds) {
-	if (propertiesFilename == null)
-	    propertiesFilename = PROPS_NAME;
-	InputStream is = NoS2JdbcManager.class.getClassLoader().getResourceAsStream(propertiesFilename);
-	if (is == null)
-	    throw new RuntimeException(new FileNotFoundException(propertiesFilename));
-	Properties ps = new Properties();
-	try {
-	    ps.load(is);
-	} catch (IOException e) {
-	    throw new IORuntimeException(e);
-	}
+        if (propertiesFilename == null)
+            propertiesFilename = PROPS_NAME;
+        InputStream is = NoS2JdbcManager.class.getClassLoader().getResourceAsStream(propertiesFilename);
+        if (is == null)
+            throw new RuntimeException(new FileNotFoundException(propertiesFilename));
+        Properties ps = new Properties();
+        try {
+            ps.load(is);
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
 
-	DataSource dataSource = ds;
+        DataSource dataSource = ds;
 
-	TransactionManager tm = TransactionManagerRegistry.get();
-	TransactionSynchronizationRegistry syncRegistry = new TransactionSynchronizationRegistryImpl(tm);
+        TransactionManager tm = TransactionManagerRegistry.get();
+        TransactionSynchronizationRegistry syncRegistry = new TransactionSynchronizationRegistryImpl(tm);
 
-	DbmsDialect dialect = new OracleDialect();;
-	String mock = rtrim(ltrim(ps.getProperty("mock")));  //for no2sjdbc-gen test
-	if (isNotBlank(mock)) {
-	    dataSource = getMockDataSource(mock);
-	} else {
-	    if (ds == null) {
-		String driverName = ps.getProperty("driverClassName");
-		String url = resolveEnvVars(ps.getProperty("URL"));
-		String user = resolveEnvVars(ps.getProperty("user"));
-		String password = resolveEnvVars(ps.getProperty("password", ""));
-		int poolSize = Integer.valueOf(resolveEnvVars(ps.getProperty("poolSize", "900")));
-		dataSource = new NoS2JdbcDataSource(tm, driverName, url, user, password, poolSize);
-	    }
-	    String dialectName = ltrim(rtrim(resolveEnvVars(ps.getProperty("dialect"))));
-	    dialect = getDialect(dialectName);
-	}
+        DbmsDialect dialect = new OracleDialect();;
+        String mock = rtrim(ltrim(ps.getProperty("mock")));  //for no2sjdbc-gen test
+        if (isNotBlank(mock)) {
+            dataSource = getMockDataSource(mock);
+        } else {
+            if (ds == null) {
+                String driverName = ps.getProperty("driverClassName");
+                String url = resolveEnvVars(ps.getProperty("URL"));
+                String user = resolveEnvVars(ps.getProperty("user"));
+                String password = resolveEnvVars(ps.getProperty("password", ""));
+                int poolSize = Integer.valueOf(resolveEnvVars(ps.getProperty("poolSize", "900")));
+                dataSource = new NoS2JdbcDataSource(tm, driverName, url, user, password, poolSize);
+            }
+            String dialectName = ltrim(rtrim(resolveEnvVars(ps.getProperty("dialect"))));
+            dialect = getDialect(dialectName);
+        }
 
-	JdbcManagerImpl jdbcManagerImpl = new JdbcManagerImpl();
-	ManagerSetter.setToJdbcManagerImpl(jdbcManagerImpl, dataSource, dialect, syncRegistry);
+        JdbcManagerImpl jdbcManagerImpl = new JdbcManagerImpl();
+        ManagerSetter.setToJdbcManagerImpl(jdbcManagerImpl, dataSource, dialect, syncRegistry);
 
-	jdbcManager = jdbcManagerImpl;
+        jdbcManager = jdbcManagerImpl;
 
-	TransactionManagerRegistry.register(tm);
+        TransactionManagerRegistry.register(tm);
     }
 
     private String resolveEnvVars(String input)
@@ -107,53 +109,62 @@ public class NoS2JdbcManager {
         m.appendTail(sb);
         return sb.toString();
     }
-    
+
     private DataSource getMockDataSource(String mock) {
-	Class<?> c;
-	try {
-	    c = Class.forName(mock);
-	} catch (ClassNotFoundException e) {
-	    throw new ClassNotFoundRuntimeException(e);
-	}
-	try {
-	    return (DataSource)c.newInstance();
-	} catch (InstantiationException e) {
-	    throw new InstantiationRuntimeException(DataSource.class, e);
-	} catch (IllegalAccessException e) {
-	    throw new IllegalAccessRuntimeException(DataSource.class, e);
-	}
+        Class<?> c;
+        try {
+            c = Class.forName(mock);
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundRuntimeException(e);
+        }
+        try {
+            return (DataSource)c.getDeclaredConstructor().newInstance();
+        } catch (InvocationTargetException e) {
+            throw new InvocationTargetRuntimeException(DataSource.class, e);
+        } catch (NoSuchMethodException e) {
+            throw new NoSuchMethodRuntimeException(DataSource.class, "getDeclaredConstructor", new Class[0], e);
+        } catch (InstantiationException e) {
+            throw new InstantiationRuntimeException(DataSource.class, e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalAccessRuntimeException(DataSource.class, e);
+        }
     }
 
     private DbmsDialect getDialect(String dialectName) {
-	Class<?> clazz;
-	try {
-	    clazz = Class.forName("org.seasar.extension.jdbc.dialect." + dialectName);
-	} catch (ClassNotFoundException e) {
-	    throw new ClassNotFoundRuntimeException(e);
-	}
-	try {
-	    return (DbmsDialect)clazz.newInstance();
-	} catch (InstantiationException e) {
-	    throw new InstantiationRuntimeException(DbmsDialect.class, e);
-	} catch (IllegalAccessException e) {
-	    throw new IllegalAccessRuntimeException(DbmsDialect.class, e);
-	} 
+        Class<?> clazz;
+        try {
+            clazz = Class.forName("org.seasar.extension.jdbc.dialect." + dialectName);
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundRuntimeException(e);
+        }
+
+        try {
+            return (DbmsDialect)clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new InstantiationRuntimeException(DbmsDialect.class, e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalAccessRuntimeException(DbmsDialect.class, e);
+        } catch (InvocationTargetException e) {
+            throw new InvocationTargetRuntimeException(DbmsDialect.class, e);
+        } catch (NoSuchMethodException e) {
+            throw new NoSuchMethodRuntimeException(DbmsDialect.class, "getDeclaredConstructor", new Class[0], e);
+        }
     }
 
     static public synchronized JdbcManager getJdbcManager() {
-	return new NoS2JdbcManager(null, null).jdbcManager;
+        return new NoS2JdbcManager(null, null).jdbcManager;
     }
 
     static public synchronized JdbcManager getJdbcManager(String propertiesFilename, DataSource ds) {
-	return new NoS2JdbcManager(propertiesFilename, ds).jdbcManager;
+        return new NoS2JdbcManager(propertiesFilename, ds).jdbcManager;
     }
 
     static public synchronized JdbcManager getJdbcManager(String propertiesFilename) {
-	return new NoS2JdbcManager(propertiesFilename, null).jdbcManager;
+        return new NoS2JdbcManager(propertiesFilename, null).jdbcManager;
     }
 
     static public synchronized JdbcManager getJdbcManager(DataSource ds) {
-	return new NoS2JdbcManager(null, ds).jdbcManager;
+        return new NoS2JdbcManager(null, ds).jdbcManager;
     }
 
 }
