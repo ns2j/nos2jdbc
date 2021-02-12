a40ad262d1cd13909f4c44a763290e3423a2d77a
igari <igari.y@gmail.com>
2021-02-12 11:47:30 +0900

    combine Mssql2005GenDialect and MssqlGenDialect. remove Mssql2005GenDialect and Potgresql81GenDialect

--- nos2jdbc-gen/src/test/java/org/seasar/extension/jdbc/gen/internal/dialect/MssqlGenDialectTest.java
+++ nos2jdbc-gen/src/test/java/org/seasar/extension/jdbc/gen/internal/dialect/MssqlGenDialectTest.java
@@ -29,91 +29,66 @@ class MssqlGenDialectTest {
 
     private MssqlGenDialect dialect = new MssqlGenDialect();
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_int() throws Exception {
+    void testGetColumnType_int() {
         ColumnType type = dialect.getColumnType("int identity", Types.OTHER);
         assertEquals("int", type.getColumnDefinition(0, 0, 0, null));
         assertEquals(Integer.class, type.getAttributeClass(0, 0, 0));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_bigint() throws Exception {
+    void testGetColumnType_bigint()  {
         ColumnType type = dialect.getColumnType("bigint identity", Types.OTHER);
         assertEquals("bigint", type.getColumnDefinition(0, 0, 0, null));
         assertEquals(Long.class, type.getAttributeClass(0, 0, 0));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_smallint() throws Exception {
+    void testGetColumnType_smallint()  {
         ColumnType type = dialect.getColumnType("smallint identity", Types.OTHER);
         assertEquals("smallint", type.getColumnDefinition(0, 0, 0, null));
         assertEquals(Short.class, type.getAttributeClass(0, 0, 0));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_tinyint() throws Exception {
+    void testGetColumnType_tinyint()  {
         ColumnType type = dialect.getColumnType("tinyint identity", Types.OTHER);
         assertEquals("tinyint", type.getColumnDefinition(0, 0, 0, null));
         assertEquals(Short.class, type.getAttributeClass(0, 0, 0));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_decimal() throws Exception {
+    void testGetColumnType_decimal()  {
         ColumnType type = dialect.getColumnType("decimal identity", Types.OTHER);
         assertEquals("decimal(10,2)", type.getColumnDefinition(0, 10, 2, null));
         assertEquals(BigDecimal.class, type.getAttributeClass(0, 10, 2));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_numeric() throws Exception {
+    void testGetColumnType_numeric()  {
         ColumnType type = dialect.getColumnType("numeric identity", Types.OTHER);
         assertEquals("numeric(10,2)", type.getColumnDefinition(0, 10, 2, null));
         assertEquals(BigDecimal.class, type.getAttributeClass(0, 10, 2));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_nchar() throws Exception {
+    void testGetColumnType_nchar()  {
         ColumnType type = dialect.getColumnType("nchar", Types.OTHER);
         assertEquals("nchar(10)", type.getColumnDefinition(10, 0, 0, null));
         assertEquals(String.class, type.getAttributeClass(10, 0, 0));
     }
 
-    /**
-     * 
-     * @throws Exception
-     */
     @Test
-    void testGetColumnType_ntext() throws Exception {
+    void testGetColumnType_ntext()  {
         ColumnType type = dialect.getColumnType("ntext", Types.OTHER);
-        assertEquals("ntext", type.getColumnDefinition(10, 0, 0, null));
+        assertEquals("nvarchar(max)", type.getColumnDefinition(10, 0, 0, null));
+        assertEquals(String.class, type.getAttributeClass(10, 0, 0));
+    }
+
+    @Test
+    void testGetColumnType_nvarchar()  {
+        ColumnType type = dialect.getColumnType("nvarchar", Types.OTHER);
+        assertEquals("nvarchar(10)", type.getColumnDefinition(10, 0, 0, null));
         assertEquals(String.class, type.getAttributeClass(10, 0, 0));
     }
 }
