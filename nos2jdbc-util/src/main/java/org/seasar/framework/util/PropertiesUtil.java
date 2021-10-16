a4d4c85b771636c16967a57989acf8fffb0a6e3d
ns2j <31337551+ns2j@users.noreply.github.com>
2021-10-16 09:01:44 +0900

    use utf-8

--- nos2jdbc-util/src/main/java/org/seasar/framework/util/PropertiesUtil.java
+++ nos2jdbc-util/src/main/java/org/seasar/framework/util/PropertiesUtil.java
@@ -17,6 +17,8 @@ package org.seasar.framework.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 import org.seasar.framework.exception.IORuntimeException;
@@ -46,7 +48,7 @@ public class PropertiesUtil {
     public static void load(Properties props, InputStream in)
             throws IORuntimeException {
         try {
-            props.load(in);
+            props.load(new InputStreamReader(in, StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new IORuntimeException(e);
         }
