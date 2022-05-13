02514d7c032601d2bc367001491cd31c821b535a
IGARI Yoshinobu <igari.y@gmail.com>
2022-05-13 14:05:39 +0900

    move package

--- nos2jdbc-util/src/test/java/org/seasar/framework/util/Hoge.java
+++ nos2jdbc-util/src/test/java/org/seasar/framework/util/Hoge.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.framework.util.tiger;
+package org.seasar.framework.util;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
