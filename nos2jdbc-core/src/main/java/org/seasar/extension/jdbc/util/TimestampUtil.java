0870bed6e6a947688931f7262738055702a28b91
ns2j <31337551+ns2j@users.noreply.github.com>
2023-08-22 16:22:18 +0900

    add to treat java.sql.Timestamp

--- nos2jdbc-core/src/main/java/org/seasar/extension/jdbc/util/TimestampUtil.java
+++ nos2jdbc-core/src/main/java/org/seasar/extension/jdbc/util/TimestampUtil.java
@@ -1,5 +1,6 @@
 package org.seasar.extension.jdbc.util;
 
+import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,6 +13,7 @@ public class TimestampUtil {
     public static Object getTimestamp(PropertyMeta pm) {
         Class<?> c = pm.getPropertyClass();
         if (c == OffsetDateTime.class) return OffsetDateTime.now();
+        if (c == Timestamp.class) return new Timestamp(new Date().getTime());
         if (pm.getTemporalType() == TemporalType.TIMESTAMP) {
             if (c == Date.class)
                 return new Date();
